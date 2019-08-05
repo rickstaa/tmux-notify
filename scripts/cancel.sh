5f7554e66b463d119618baa6079e7dd324979a6f
Rick <rick.staa@outlook.com>
2019-08-05 18:04:28 +0200

    Added extra options and fixed error
    = Fixes =
    When trying to cancel a monitor session without first having cancelled
    one I got a bash error message. I solved this by catching the error in
    the `cancel.sh` script.
    
    = New features =
    I added 3 new features to the t-notify plugin.
    - Ability to enable a verbose notification message
    - Ability to set the verbose notification message
    - Ability to change the monitor sleep duration.

--- scripts/cancel.sh
+++ scripts/cancel.sh
@@ -4,9 +4,17 @@
 PANEID=$(tmux list-panes | grep "active" | awk -F\] '{print $3}' | awk '{print $1}')
 PID_DIR=~/.tmux/notify
 
+# Cancel monitor process if active
+{ # Try
+
 # consult pid file for the pid
 PID=$(cat "$PID_DIR/$PANEID".pid)
-kill $PID
 
-# job done - remove pid file
+# job done - kill process and remove pid file
+kill $PID
 rm "$PID_DIR/$PANEID".pid
+
+} || { # Catch
+  tmux display-message "Pane not monitored..."
+  exit 0
+}
