7d3b61de9572ea97004480a64dec938903cdc9c6
Rick <rick.staa@outlook.com>
2019-08-05 18:00:44 +0200

    dded extra options and fixed error
    = Fixes =
    When trying to cancel a monitor session without first having cancelled
    one I got a bash error message. I solved this by catching the error in
    the `cancel.sh` script.
    
    = New features =
    I added 3 new features to the t-notify plugin.
    - Ability to enable a verbose notification message
    - Ability to set the verbose notification message
    - Ability to change the monitor sleep duration.

---
+++ scripts/variables.sh
@@ -0,0 +1,13 @@
+SUPPORTED_VERSION="1.9"
+
+## Tnotify tmux options
+
+# Notification verbosity settings
+verbose_option="@tnotify-verbose"
+verbose_default="off"
+verbose_msg_option="@tnotify-verbose-msg"
+verbose_msg_default="(#S, #I:#P) Tmux pane task completed!"
+
+# Monitor checker interval
+monitor_sleep_duration="@tnotify-sleep-duration"
+monitor_sleep_duration_default=10
